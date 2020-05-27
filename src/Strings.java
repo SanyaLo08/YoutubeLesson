70cca56e4f7e2d0018622da4ef5cc250860ae2ef
Alexander <9265501657@mail.ru>
2020-05-28 01:03:30 +0300

    Немного практики, вспоминалки!

---
+++ src/Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args) {
+        int x = 7;
+        String s = "Приветули";
+        String space = " ";
+        String name = "SanyaLo";
+        System.out.println(s + space + name);
+        System.out.println("SanyaLo" + " " + "Приветули"); // или так можно
+        System.out.println("Число " + x + ", говорят счастливое :)");
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables { // Переменные
+    public static void main(String[] args) {
+        int myInt;
+        myInt = 777; // или можно сразу myInt = 777;
+        short myShort = 32666;
+        long myLong = 26637447;
+        double myDouble = 235.35;
+        float myFloat = 2345.5f;
+        char myChar = 'a';
+        boolean b = true;
+        byte myByte = 127; // от -128 до 127;
+
+        System.out.println(myShort);
+
+    }
+}
